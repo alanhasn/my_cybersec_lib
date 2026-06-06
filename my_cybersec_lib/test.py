b83b2c87543af4ad15cacef1d7e8444a5efdf59b
ebdulah <ebdulahmuhemed0@gmail.com>
2026-06-06 14:17:18 +0300

    ebdulah add line

--- my_cybersec_lib/test.py
+++ my_cybersec_lib/test.py
@@ -3,3 +3,4 @@ from SecureTool.Password import PasswordStrengthChecker
 checker = PasswordStrengthChecker(username="alan")
 result = checker.check_strength("pass1234!")
 print(result)
+# ebdulah miuhemed add a new line to test the code
